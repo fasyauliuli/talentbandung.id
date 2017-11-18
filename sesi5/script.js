de109ed075db1eb396d2b3f662e379ccec567d34
Aulia Fasya Nugroho <liuli.fasya@gmail.com>
2017-11-18 09:26:05 +0700

    json change

--- sesi5/script.js
+++ sesi5/script.js
@@ -3,10 +3,7 @@ $.get('https://ariona.net/talentbandung/', function(data, status) {
 
     for (i=0; i < data.length; i++){
         //console.log(data);
-        var judulTaman = data[i].nama;
-        var pisah = judulTaman.substr(0, judulTaman.indexOf(' '));
-        var gabung = judulTaman.substr(judulTaman.indexOf(' '));
-        var tamanItem = '<div class="grid-item"><div class="taman"><figure><a href="single.html"><img src=' + data[i].image.thumbnail + '>' + '</a></figure><div class="taman-detail"><h2><a href = "single.html"><span class ="subtitle">' + pisah + '</span><span class="title">' + gabung + '</a></h2><p>' + data[i].alamat + '</p></div></div></div>';
+        var tamanItem = '<div class="grid-item"><div class="taman"><figure><a href="single.html"><img src=' + data[i].image.thumbnail + '>' + '</a></figure><div class="taman-detail"><h2><a href = "single.html"><span class ="subtitle">' + "Taman" + '</span><span class="title">' + data[i].nama + '</a></h2><p>' + data[i].alamat + '</p></div></div></div>';
         listTaman.append(tamanItem);
     }
 
