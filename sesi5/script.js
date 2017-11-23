077e296968a4ea1e70843cf4eefac96f5ef52759
Aulia Fasya Nugroho <liuli.fasya@gmail.com>
2017-11-23 19:16:31 +0700

    assets
    upload image, styling setiap package

--- sesi5/script.js
+++ sesi5/script.js
@@ -2,7 +2,7 @@ $.get('https://ariona.net/talentbandung/', function(data, status) {
     var listTaman = $('.daftar-taman');
 
     for (i=0; i < data.length; i++){
-        //console.log(data);
+        console.log(data);
         var tamanItem = '<div class="grid-item"><div class="taman"><figure><a href="single.html"><img src=' + data[i].image.thumbnail + '>' + '</a></figure><div class="taman-detail"><h2><a href = "single.html"><span class ="subtitle">' + "Taman" + '</span><span class="title">' + data[i].nama + '</a></h2><p>' + data[i].alamat + '</p></div></div></div>';
         listTaman.append(tamanItem);
     }
